39657970f709697e69ebaf1dee12ec9372e3ebb6
Nisarga Patel <np8@umbc.edu>
2017-08-28 00:13:08 -0400

    Removed miscellaneous comments

--- App.js
+++ App.js
@@ -9,38 +9,3 @@ export default class App extends React.Component {
     )
   }
 }
-// export default class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       isReady: false,
-//       currentPriceUSD: 0
-//     }
-//   }
-//
-//   async componentWillMount() {
-//     await Expo.Font.loadAsync({
-//       Roboto: require("native-base/Fonts/Roboto.ttf"),
-//       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-//       Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
-//     });
-//
-//     this.setState({ isReady: true });
-//   }
-//   render() {
-//     const fontsNotRendered = (
-//       <View>
-//         <Text>Fonts loading ...</Text>
-//       </View>
-//     );
-//
-//     if (!this.state.isReady) {
-//       return (fontsNotRendered);
-//     }
-//     return (
-//        StackNavigator({
-//           Home: { screen: Home }
-//         });
-//     );
-//   }
-// }
